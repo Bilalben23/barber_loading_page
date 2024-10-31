c55a59f91651825a36d78c164a465d8d913402f2
Bilal Benyoussef <bilalbeny735@gmail.com>
2024-10-31 14:54:09 +0100

    The initial of the app

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{ts,js,tsx,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ],
+}
+
