d1030cc3df07bb1e88dc345373b4a1d561cd19b0
Bilal Benyoussef <bilalbeny735@gmail.com>
2024-10-31 21:28:59 +0100

    make the header responsive

--- src/components/SmallMenu.jsx
+++ src/components/SmallMenu.jsx
@@ -1,21 +1,39 @@
+import React, { useState } from 'react'
+import { FaArrowRight, FaBars, FaCalendarAlt, FaEnvelope, FaHome, FaInfoCircle, FaServicestack } from 'react-icons/fa'
 import { Link as ScrollLink } from 'react-scroll';
-import { FaCalendarAlt, FaEnvelope, FaHome, FaInfoCircle, FaServicestack } from "react-icons/fa"
-import logo from "../assets/img3.svg"
 import backgroundImg from "../assets/img1.png"
-import SmallMenu from './SmallMenu';
 
-export default function Header() {
+
+export default function SmallMenu() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    function handleMenuToggle() {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    function closeMenu() {
+        setIsMenuOpen(false)
+    }
+
+
     return (
-        <header
-            className='fixed top-0 z-10 flex items-center justify-between w-full px-8 py-1 border-b border-gray-900'
-            style={{ background: `url(${backgroundImg}) center no-repeat`, backgroundSize: "cover" }}
-        >
-            <div>
-                <img src={logo} alt="logo" className='w-16 h-16' />
-            </div>
-            <div className='items-center hidden md:flex gap-x-10'>
-                <nav>
-                    <ul className='text-lg menu menu-horizontal'>
+        <div className='flex items-center md:hidden gap-x-10'>
+
+            <button type='button' className={`btn btn-circle btn-ghost ${isMenuOpen && "btn-active"}`} onClick={handleMenuToggle}>
+                {
+                    isMenuOpen
+                        ? <FaArrowRight size={27} />
+                        : <FaBars size={30} />
+                }
+
+            </button>
+
+            <div
+                className={`fixed right-0 top-[85px] h-[calc(100vh-88px)] w-full p-5 transition-all duration-300 ${isMenuOpen ? "translate-x-0" : "translate-x-full"}`}
+                style={{ background: `url(${backgroundImg}) center no-repeat`, backgroundSize: "cover" }}
+            >
+                <nav >
+                    <ul className='w-full menu menu-vertical *:text-lg' >
                         <li>
                             <ScrollLink
                                 to="home"
@@ -23,6 +41,7 @@ export default function Header() {
                                 smooth={true}
                                 offset={-70}
                                 duration={500}
+                                onClick={closeMenu}
                             >
                                 <FaHome />
                                 Home
@@ -61,22 +80,20 @@ export default function Header() {
                                 duration={500}
                             >
                                 <FaEnvelope />
-
                                 Contact
                             </ScrollLink>
                         </li>
                     </ul>
                 </nav>
-                <div>
-                    <button type='button' className='flex items-center rounded-full btn-outline btn btn-secondary'>
+                <div className='pl-5 mt-4'>
+                    <button type='button' className='flex items-center h-[50px] rounded-full btn-outline btn btn-sm btn-secondary'>
                         <FaCalendarAlt size={20} />
                         Book Now
                     </button>
                 </div>
             </div>
-
-            <SmallMenu />
-
-        </header>
+        </div>
     )
 }
+
+// !I tried to apply the event delegation, it doesn't work.
