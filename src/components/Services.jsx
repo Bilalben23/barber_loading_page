366dbaef9e13038f8fd90f9b0458bdc156d09d04
Bilal Benyoussef <bilalbeny735@gmail.com>
2024-11-02 22:04:46 +0100

    add sound effect when click on service card icon

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { services } from "../data/services"
 import ServiceIconCard from '../cards/ServiceIconCard'
 import ServiceDetailsCard from "../cards/ServiceDetailsCard"
+import clickSnap from "../assets/click-snap.wav"
 
 export default function Services() {
 
     const [selectedServiceId, setSelectedServiceId] = useState(1)
-
+    const audioRef = useRef(null);
 
     function handleSelectedService(id) {
         setSelectedServiceId(id)
+        if (audioRef.current) {
+            audioRef.current.play();
+        }
     }
 
     const selectedService = services.find(service => service.id === selectedServiceId)
@@ -23,6 +27,10 @@ export default function Services() {
             </div>
             <div className='w-[90%] mx-auto mt-8'>
                 <div className='w-[95%] md:w-[90%] mx-auto'>
+                    <audio ref={audioRef}>
+                        <source src={clickSnap} type="audio/wav" />
+                        <p>Your browser does not support the audio element.</p>
+                    </audio>
                     <div className='w-full py-2 overflow-x-auto shadow-inner carousel-center md:carousel-start carousel gap-x-4'>
                         {
                             services.map(service => <ServiceIconCard
